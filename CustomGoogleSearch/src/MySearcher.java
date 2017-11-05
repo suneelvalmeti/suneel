e6e5028e348be7a408c215f4487b425b509a72e0
DevaRaju <DevaRaju@WINCTRL-550A8MK>
2017-11-05 20:04:58 +0530

    edited url

--- CustomGoogleSearch/src/MySearcher.java
+++ CustomGoogleSearch/src/MySearcher.java
@@ -9,7 +9,7 @@ public class MySearcher extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		String name = request.getParameter("name");
-		response.sendRedirect("https://www.google.co.in/#q=" + name);
+		response.sendRedirect("https://www.facebook.com/#q=" + name);
 	}
 }//q=hello&oq=hello&aqs=chrome..69i57j69i61l2j0l3.986j0j7&sourceid=chrome&ie=UTF-8
 //http://localhost:9998/MySearcher?name=hello
